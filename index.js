83e933011e56a6cf2cf495d5d8e710ccc7e23a6b
Alex Lunyov <isuntc@gmail.com>
2018-11-02 18:32:01 +0800

    Initial version

---
+++ index.js
@@ -0,0 +1,23 @@
+function runWithFps (fn, fps) {
+  var interval = 1000 / fps;
+  var then = Date.now();
+  var stopped = false;
+
+  function run() {
+    requestAnimationFrame(run);
+
+    var now = Date.now();
+    var delta = now - then;
+
+    if (delta > interval && !stopped) {
+      then = now - (delta % interval);
+      fn(now);
+    }
+  };
+
+  run();
+
+  return function() {
+    stopped = true;
+  }
+};
